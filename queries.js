daf6f5ef0f00d280c8f33f9ade401b9e62462414
justcallmebeef <bethanymitch10@gmail.com>
2021-03-06 13:51:09 -0700

    commented out code

--- queries.js
+++ queries.js
@@ -4,31 +4,31 @@ module.exports = {
   listAllCompanies() {
     return database("companies");
   },
-  getCompaniesById(id) {
-    return database("companies").where("id", id).first();
-  },
-  createCompany(company) {
-    return database("companies").insert(company).returning("*");
-  },
-  deleteCompany(id) {
-    return database("companies").where("id", id).delete();
-  },
-  updateCompany(id, company) {
-    return database("companies").where("id", id).update(company).returning("*");
-  },
+  // getCompaniesById(id) {
+  //   return database("companies").where("id", id).first();
+  // },
+  // createCompany(company) {
+  //   return database("companies").insert(company).returning("*");
+  // },
+  // deleteCompany(id) {
+  //   return database("companies").where("id", id).delete();
+  // },
+  // updateCompany(id, company) {
+  //   return database("companies").where("id", id).update(company).returning("*");
+  // },
   listAllFounders() {
     return database("founders");
   },
-  getFoundersById(id) {
-    return database("founders").where("id", id).first();
-  },
-  createFounder(founder) {
-    return database("founders").insert(founder).returning("*");
-  },
-  deleteFounder(id) {
-    return database("founders").where("id", id).delete();
-  },
-  updateFounder(id, founder) {
-    return database("founders").where("id", id).update(founder).returning("*");
-  },
+  // getFoundersById(id) {
+  //   return database("founders").where("id", id).first();
+  // },
+  // createFounder(founder) {
+  //   return database("founders").insert(founder).returning("*");
+  // },
+  // deleteFounder(id) {
+  //   return database("founders").where("id", id).delete();
+  // },
+  // updateFounder(id, founder) {
+  //   return database("founders").where("id", id).update(founder).returning("*");
+  // },
 };
