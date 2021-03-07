2559e398b01119c2d228806f5e1814fe147e46bf
justcallmebeef <bethanymitch10@gmail.com>
2021-03-07 15:36:23 -0700

    rm code that was not being used"
    ;

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const queries = require("./queries");
 const port = process.env.PORT || 3002;
 
 app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
