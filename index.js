e29f22f7b07454d3428bb7993cb26fa9ff222d8b
justcallmebeef <bethanymitch10@gmail.com>
2021-03-07 08:57:14 -0700

    added headers

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
