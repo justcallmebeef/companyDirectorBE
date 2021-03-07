948c3fe9248565198af2a58cf19f7188d25c9f24
justcallmebeef <bethanymitch10@gmail.com>
2021-03-07 10:17:36 -0700

    moved cors

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const queries = require("./queries");
 const port = process.env.PORT || 3002;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
